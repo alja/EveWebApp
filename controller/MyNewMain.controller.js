e49e6b954b4f3067a1f48470387d1964b3be3905
alja <amraktadel@ucsd.edu>
2019-07-18 14:03:08 -0700

    First version of custom app using currentdir

---
+++ controller/MyNewMain.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define(['rootui5/eve7/controller/Main.controller',
+               'rootui5/eve7/lib/EveManager'
+], function(MainController, EveManager) {
+   "use strict";    
+
+   return MainController.extend("currentdir.MyNewMain", {                    
+
+      onInit: function() {
+                       MainController.prototype.onInit.apply(this, arguments);
+      },
+            
+      showHelp : function(oEvent) {
+         alert("=====User support: fireworks@cern.ch");
+      }
+   });
+});
---
+++ controller/MyNewSummary.controller.js
@@ -0,0 +1,48 @@
+sap.ui.define(['rootui5/eve7/controller/Summary.controller',
+               'rootui5/eve7/lib/EveManager'
+], function(SummaryController, EveManager) {
+   "use strict";    
+
+   return SummaryController.extend("currentdir.MyNewSummary", {                    
+
+      onInit: function() {
+                       SummaryController.prototype.onInit.apply(this, arguments);
+      },
+       
+
+      createSummaryModel: function(tgt, src) {
+         if (tgt === undefined) {
+            tgt = [];
+            src = this.mgr.childs[0].childs[2].childs;
+            console.log('original model', src);
+            for (var i = 0; i < src.length; i++) {
+               if (src[i].fName == "Collections") {
+                  src = src[i].childs;
+                  console.log("got ", src);
+               }
+            }
+         }
+         for (var n=0;n<src.length;++n) {
+            var elem = src[n];
+
+            var newelem = { fName: elem.fName, id: elem.fElementId, fHighlight: "None", fBackground: "" };
+
+            if (this.canEdit(elem))
+               newelem.fType = "DetailAndActive";
+            else
+               newelem.fType = "Active";
+
+            newelem.masterid = elem.fMasterId || elem.fElementId;
+
+            tgt.push(newelem);
+            if ((elem.childs !== undefined) && this.anyVisible(elem.childs))
+               newelem.childs = this.createSummaryModel([], elem.childs);
+         }
+
+         return tgt;
+      },
+      addCollection: function (evt){
+         alert("add collection");
+      }
+   });
+});
