4eb1d1e7b4102142a36dc3848994b11d56b20b6c
alja <amraktadel@ucsd.edu>
2022-03-09 09:34:50 -0800

    remove obsoelte includes

--- collection_proxies_test.C
+++ collection_proxies_test.C
@@ -1,42 +1,4 @@
-/// \file
-/// \ingroup tutorial_eve7
-///
-/// This is an example of visualization of containers
-/// with REveDataCollection and REveDataProxyBuilders.
-/// \macro_code
-///
 
-
-#include "ROOT/REveDataTable.hxx"
-#include "ROOT/REveDataSimpleProxyBuilderTemplate.hxx"
-#include "ROOT/REveManager.hxx"
-#include "ROOT/REveScalableStraightLineSet.hxx"
-#include "ROOT/REveViewContext.hxx"
-#include <ROOT/REveGeoShape.hxx>
-#include <ROOT/REveJetCone.hxx>
-#include <ROOT/REvePointSet.hxx>
-#include <ROOT/REveProjectionBases.hxx>
-#include <ROOT/REveProjectionManager.hxx>
-#include <ROOT/REveScene.hxx>
-#include <ROOT/REveTableProxyBuilder.hxx>
-#include <ROOT/REveTableInfo.hxx>
-#include <ROOT/REveTrack.hxx>
-#include <ROOT/REveTrackPropagator.hxx>
-#include <ROOT/REveViewer.hxx>
-#include <ROOT/REveViewContext.hxx>
-#include <ROOT/REveBoxSet.hxx>
-#include <ROOT/REveSelection.hxx>
-#include <ROOT/REveCalo.hxx>
-
-#include "TGeoTube.h"
-#include "TROOT.h"
-#include "TList.h"
-#include "TParticle.h"
-#include "TRandom.h"
-#include "TApplication.h"
-#include "TFile.h"
-#include "TH2F.h"
-#include <iostream>
 
 #include "collection_proxies.C"
 
