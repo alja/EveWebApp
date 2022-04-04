3d6b02b414d2a8a43c95d7102927c199b478dd79
alja <amraktadel@ucsd.edu>
2022-04-04 14:23:47 -0700

    Adjust to jsroot ES6 modules and classes

--- ui5/controller/MyNewSummary.controller.js
+++ ui5/controller/MyNewSummary.controller.js
@@ -6,8 +6,10 @@ sap.ui.define(['rootui5/eve7/controller/Summary.controller',
                "sap/m/Text",
 	       "sap/m/ColumnListItem",
 	       "sap/ui/model/Sorter",
-   "rootui5/eve7/controller/Ged.controller"
-              ], function(SummaryController, EveManager, JSONModel, Table, Column, Text,ColumnListItem, Sorter, GedController) {
+		   "sap/ui/layout/SplitterLayoutData",
+   "rootui5/eve7/controller/Ged.controller",
+   "sap/ui/core/mvc/XMLView"
+              ], function(SummaryController, EveManager, JSONModel, Table, Column, Text,ColumnListItem, Sorter,SplitterLayoutData, GedController, XMLView) {
    "use strict";
 
    return SummaryController.extend("custom.MyNewSummary", {
@@ -29,14 +31,53 @@ sap.ui.define(['rootui5/eve7/controller/Summary.controller',
          this.summaryElements = {};
 
          var src = this.mgr.childs[0].childs[2].childs;
-		 /*
-         for (var i = 0; i < src.length; i++) {
-            if (src[i].fName == "Collections")
-               src = src[i].childs;
-         }*/
 
-         return this.createSummaryModel([], src, "/");
+		 let tgt = [];
+
+		  for (var i = 0; i < src.length; i++) {
+			  if (src[i].fName == "Collections") {
+				  let x  = src[i].childs;
+				  this.createSummaryModel(tgt, x, "/");
+			  }
+			  if (src[i].fName == "Associations") {
+				  let x = src[i].childs;
+				 return this.createSummaryModel(tgt, x, "/");
+			  }
+		  }
+
+       //  return this.createSummaryModel([], src, "/");
       },
+
+
+      showGedEditor: function(elementId) {
+
+		var sumSplitter = this.byId("sumSplitter");
+
+		if (!this.ged) {
+		   var pthis = this;
+
+		   XMLView.create({
+			  viewName: "rootui5.eve7.view.Ged",
+			  layoutData: new SplitterLayoutData("sld", {size: "30%"}),
+			  height: "100%"
+		   }).then(function(oView) {
+			  pthis.ged = oView;
+			  pthis.ged.getController().setManager(pthis.mgr);
+			  pthis.ged.getController().buildFWAssociationSetter = function(el)
+			  {
+				this.makeBoolSetter(el.fRnrSelf, "xxx");
+				this.makeStringSetter(el.fFilterExpr, "Filter", "SetFilterExpr");
+			  };
+
+
+			  pthis.ged.getController().showGedEditor(sumSplitter, elementId);
+
+		   });
+		} else {
+		   this.ged.getController().showGedEditor(sumSplitter, elementId);
+		}
+	 },
+
 
       addCollection: function (evt){
          if (!this.table)
