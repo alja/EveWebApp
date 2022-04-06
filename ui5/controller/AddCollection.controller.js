7d519752b81bbd630b1d13c6b3cad54fb476cbab
alja <amraktadel@ucsd.edu>
2022-04-06 14:25:35 -0700

    Move AddCollection to a new controlle, add new tables into tabs

---
+++ ui5/controller/AddCollection.controller.js
@@ -0,0 +1,132 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Sorter",
+    "sap/m/Column",
+    "sap/m/Button",
+    "sap/m/Text"
+], function (Controller, JSONModel, Sorter, Column, Button, Text) {
+    "use strict";
+
+
+
+    var AddCollectionController = Controller.extend("custom.controller.AddCollection", {
+
+        onInit: function () {
+            let data = {
+                names: [
+                    { firstName: "Peter", lastName: "Mueller" },
+                    { firstName: "Petra", lastName: "Maier" },
+                    { firstName: "Thomas", lastName: "Smith" },
+                    { firstName: "John", lastName: "Williams" },
+                    { firstName: "Maria", lastName: "Jones" }
+                ],
+                friends: [
+                    { firstName: "Feter", lastName: "Mueller" },
+                    { firstName: "Fetra", lastName: "Maier" },
+                    { firstName: "Tsssshomas", lastName: "Smith" },
+                    { firstName: "Jack", lastName: "Williams" },
+                    { firstName: "Yumi", lastName: "Jones" }
+                ]
+            };
+            let oModel = new sap.ui.model.json.JSONModel();
+            // set the data for the model
+            oModel.setData(data);
+            this.getView().setModel(oModel);
+
+            let cTable = this.makeTable("/names");
+            this.getView().byId("ctab").addContent(cTable);
+
+           let aTable = this.makeTable("/friends");
+            this.getView().byId("atab").addContent(aTable);
+
+            this.getView().setModel(oModel);
+
+            this.dialog = this.getView().byId("acdialog");
+
+            let pthis = this;
+            let beginButton = new sap.m.Button('simpleDialogAcceptButton', { text: "AddEntry", press: function () { pthis.addCollection(); } });
+            let endButton = new sap.m.Button('simpleDialogCancelButton', { text: "Cancel", press: function () { pthis.close(); } });
+
+            this.dialog.setEndButton(endButton);
+            this.dialog.setBeginButton(beginButton);
+
+        },
+
+        onFilterPost: function (oEvent) {
+            var txt = oEvent.getParameter("query");
+            let filter = new sap.ui.model.Filter([new sap.ui.model.Filter("firstName", sap.ui.model.FilterOperator.Contains, txt), new sap.ui.model.Filter("lastName", sap.ui.model.FilterOperator.Contains, txt)], false);
+            this.getTable().getBinding("items").filter(filter, "Applications");
+        },
+
+        getTable: function()
+        {
+            let tt = this.getView().byId("tt");
+            let si = tt.getSelectedItem();
+            let t = this.getView().byId(si);
+            return t.getContent()[1];
+        },
+
+        makeTable: function (dpath) {
+
+            var oTable = new sap.m.Table( {
+                mode: "SingleSelect",
+                columns: [
+                    new sap.m.Column({ header: new Text({ text: "First Name" }) }),
+                    new sap.m.Column({ header: new Text({ text: "Lasr Name" }) })
+                ]
+            });
+
+            oTable.setIncludeItemInSelection(true);
+            oTable.bActiveHeaders = true;
+            // oTable.setModel(oModel);
+
+
+            oTable.attachEvent("columnPress", function (evt) {
+                var col = evt.getParameters().column;
+                let descend = false;
+                let ch = this.getColumns()[col._index];
+                let ar = ["firstName", "lastName"];
+
+                // init first time ascend
+                if (col.getSortIndicator() == sap.ui.core.SortOrder.Descending || col.getSortIndicator() == sap.ui.core.SortOrder.None) {
+                    descend = true;
+                }
+                else {
+                    descend = false;
+                }
+                // flip the state
+                descend = !descend;
+                var oSorter = new Sorter(ar[col._index - 1], descend);
+                var oItems = this.getBinding("items");
+                oItems.sort(oSorter);
+
+                var indicator = descend ? sap.ui.core.SortOrder.Descending : sap.ui.core.SortOrder.Ascending;
+                col.setSortIndicator(indicator);
+            });
+
+            oTable.bindItems({
+                path: dpath,
+                template: new sap.m.ColumnListItem({
+                    cells: [
+                        new sap.m.Text({ text: "{firstName}" }),
+                        new sap.m.Text({ text: "{lastName}" })
+                    ]
+                })
+            });
+            oTable.addStyleClass("sapUiSizeCompact");
+            return oTable;
+        },
+
+        addCollection: function () {
+            let pt = this.getTable();
+            var oSelectedItem = pt.getSelectedItems();
+            var item1 = oSelectedItem[0];
+            console.log("SELECT ", item1.getBindingContext().getObject());
+        },
+
+    });
+
+    return AddCollectionController;
+
+});
--- ui5/controller/MyNewSummary.controller.js
+++ ui5/controller/MyNewSummary.controller.js
@@ -76,116 +76,22 @@ sap.ui.define(['rootui5/eve7/controller/Summary.controller',
 		} else {
 		   this.ged.getController().showGedEditor(sumSplitter, elementId);
 		}
-	 },
+	   },
 
 
-      addCollection: function (evt){
-         if (!this.table)
-            this.createTable();
-
-         if (!this.popover) {
-            this.popover = new sap.m.Popover("popupTable", {title:"Popup TEST"});
-
-
-	    let sw = new sap.m.SearchField();
-	    sw.placeholder="Filter";
-	    var pt = this.table;
-	    sw.attachSearch(function(oEvent) {
-	       var txt = oEvent.getParameter("query");
-	       let filter = new sap.ui.model.Filter([new sap.ui.model.Filter("firstName", sap.ui.model.FilterOperator.Contains, txt), new sap.ui.model.Filter("lastName", sap.ui.model.FilterOperator.Contains, txt)],false);
-	       pt.getBinding("items").filter(filter, "Applications");
-	    });
-
-            this.popover.addContent(sw);
-            this.popover.addContent(this.table);
-
-	    // footer
-	    var pthis = this;
-	    let fa = new sap.m.OverflowToolbar();
-	    let b1 = new sap.m.Button({text:"AddCollection"});
-	    fa.addContent(b1);
-	    b1.attachPress(function(oEvent) {
-               var oSelectedItem = pt.getSelectedItems();
-	       var item1 = oSelectedItem[0];
-	       console.log("SELECT ",item1.getBindingContext().getObject());
-	    });
-
-	    let b2 = new sap.m.Button({text:"Close"});
-	    fa.addContent(b2);
-	    b2.attachPress(function(oEvent) {
-	       pthis.popover.close();
-	    });
-	    this.popover.setFooter(fa);
-         }
-         this.popover.openBy(this.byId("addCollection"));
-      },
-
-
-      createTable: function() {
-	 // create some dummy JSON data
-	 var data = {
-	    names: [
-	       {firstName: "Peter", lastName: "Mueller"},
-	       {firstName: "Petra", lastName: "Maier"},
-	       {firstName: "Thomas", lastName: "Smith"},
-	       {firstName: "John", lastName: "Williams"},
-	       {firstName: "Maria", lastName: "Jones"}
-	    ]
-	 };
-	 // create a Model with this data
-	 var model = new JSONModel();
-	 model.setData(data);
-
-
-	 // create the UI
-
-	 // create a sap.m.Table control
-	 var table = new Table("tableTest",{
-	    mode:"SingleSelect",
-	    columns: [
-	       new Column("lastName", {header: new Text({text: "Last Name"})}),
-	       new Column("firstName", {header: new Text({text:"First Name"})})
-	    ]
-	 });
-	 table.setIncludeItemInSelection(true);
-         this.table = table;
-	 table.bActiveHeaders = true;
-
-	 table.attachEvent("columnPress", function(evt) {
-	    console.log("press paramerer", evt.getParameters());
-            var col = evt.getParameters().column;
-	    var sv = false;
-
-	    // init first time ascend
-	    if (col.getSortIndicator() == sap.ui.core.SortOrder.Descend || col.getSortIndicator() == sap.ui.core.SortOrder.None ) {
-	       sv = true;
-	    }
-	    else {
-               sv = false;
-	    }
-
-	    var oSorter = new Sorter(col.sId, sv);
-	    var oItems = this.getBinding("items");
-	    oItems.sort(oSorter);
-
-	    var indicator = sv ?  sap.ui.core.SortOrder.Descending :  sap.ui.core.SortOrder.Ascending;
-	    col.setSortIndicator(indicator);
-	 });
-
-
-	 // bind the Table items to the data collection
-	 table.bindItems({
-	    path : "/names",
-	    template : new ColumnListItem({
-	       cells: [
-		  new sap.m.Text({text: "{lastName}"}),
-		  new sap.m.Text({text: "{firstName}"})
-	       ]
-	    })
-	 });
-	 // set the model to the Table, so it knows which data to use
-	 table.setModel(model);
-
-      }
+	   addCollection: function (evt) {
+		   if (this.acGUI) {
+			   this.acGUI.open();
+		   }
+		   else {
+			   let pthis = this;
+			   XMLView.create({
+				   viewName: "custom.view.AddCollection",
+			   }).then(function (oView) {
+				   pthis.acGUI = oView.getController().dialog;
+				   pthis.acGUI.open();
+			   });
+		   }
+	   }
    });
 });
